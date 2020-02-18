7611bdfa5604c545cdb3cb006ac851b21fae2e4a
André Vital <santiago_vital@outlook.com>
2020-02-18 14:40:14 -0600

    Added class period advancing

--- newPeriod.php
+++ newPeriod.php
@@ -5,11 +5,12 @@
     date_default_timezone_set('America/Mexico_City');
 
     $subject = $_REQUEST['newPeriodClass'];
+    $userID = $_REQUEST['currentUser'];
 
-    if($subject == 69) $queryNewPeriod = "Update materias set materia_parcialActual = materia_parcialActual + 1 where materia_ID > 0";
-    else $queryNewPeriod = "Update materias set materia_parcialActual = materia_parcialActual + 1 where materia_ID = $subject";
+    if($subject == 69) $queryNewPeriod = "Update materias set materia_parcialActual = materia_parcialActual + 1 where materia_ID > 0 and materia_usuario = $usr";
+    else $queryNewPeriod = "Update materias set materia_parcialActual = materia_parcialActual + 1 where materia_ID = $subject and materia_usuario = $usr";
     
     $resultNewPeriod = mysqli_query($connect, $queryNewPeriod);
 	
-	echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
+	echo '<meta http-equiv="refresh" content="0;URL=index_user.php"/>';
 ?>
